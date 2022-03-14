337aea9622e1d9dc166004f9f1aa610f46470050
なるみ <narumi@maicoin.com>
2022-03-14 23:17:01 +0800

    Test get_max_ws_uri

---
+++ tests/test_utils.py
@@ -0,0 +1,12 @@
+import os
+
+from maicoin.utils import MAX_WS_URI
+from maicoin.utils import get_max_ws_uri
+
+
+def test_get_max_ws_uri():
+    assert get_max_ws_uri() == MAX_WS_URI
+
+    uri = 'wss://max-stream.maicoin.com/ws'
+    os.environ['MAX_WS_URI'] == uri
+    assert get_max_ws_uri() == uri
