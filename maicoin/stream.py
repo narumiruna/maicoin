90f15681ebfdf6d506ebeb1397368a71355bd9c1
narumi <toucans-cutouts0f@icloud.com>
2024-03-29 19:43:09 +0800

    rename

--- maicoin/stream.py
+++ maicoin/stream.py
@@ -8,9 +8,9 @@ from typing import Callable
 from loguru import logger
 from websockets.client import connect
 
-from .data import Action
-from .data import Event
-from .data import Subscription
+from .base import Action
+from .base import Event
+from .base import Subscription
 
 MAX_WS_URI = os.environ.get("MAX_WS_URI", "wss://max-stream.maicoin.com/ws")
 
