99fecd458a36305ba848cadb9780b2ad430333c9
narumi <toucans-cutouts0f@icloud.com>
2026-05-08 22:08:46 +0800

    feat(v3): add public REST endpoints

--- src/maicoin/v3/__init__.py
+++ src/maicoin/v3/__init__.py
@@ -7,13 +7,35 @@ from .client import DEFAULT_TIMEOUT
 from .client import Client
 from .errors import MaxAPIError
 from .errors import MaxHTTPError
+from .models import Currency
+from .models import CurrencyNetwork
+from .models import Depth
+from .models import HistoricalIndexPrice
+from .models import InterestRate
+from .models import KLine
+from .models import Market
+from .models import PublicTrade
+from .models import Staking
+from .models import Ticker
+from .models import Timestamp
 
 __all__ = [
     "BASE_URL",
     "DEFAULT_TIMEOUT",
     "Client",
+    "Currency",
+    "CurrencyNetwork",
+    "Depth",
+    "HistoricalIndexPrice",
+    "InterestRate",
+    "KLine",
+    "Market",
     "MaxAPIError",
     "MaxHTTPError",
+    "PublicTrade",
+    "Staking",
+    "Ticker",
+    "Timestamp",
     "build_auth_headers",
     "encode_payload",
     "generate_nonce",
